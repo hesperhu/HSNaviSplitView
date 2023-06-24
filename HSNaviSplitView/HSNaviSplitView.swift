4308790f2dd0d4ca81a2589a72f4028b0128711e
hu shouwen <hesper.hu@gmail.com>
2023-06-24 16:07:13 +0800

    add fish data and use it

---
+++ HSNaviSplitView/HSNaviSplitView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  HSNaviSplitView
+//
+//  Created by hsw on 2023-06-24.
+//
+
+import SwiftUI
+import HSModle
+
+struct HSNaviSplitView: View {
+    @State var select: Dish? = nil
+    var body: some View {
+        NavigationSplitView {
+            List(HSData.dishes, id: \.id, selection: $select) { dish in
+                NavigationLink(dish.name, value: dish)
+            }
+        } detail: {
+            if let dish = select {
+                Text(dish.detail)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HSNaviSplitView()
+    }
+}
--- HSNaviSplitView/HSNaviSplitViewApp.swift
+++ HSNaviSplitView/HSNaviSplitViewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HSNaviSplitViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HSNaviSplitView()
         }
     }
 }
