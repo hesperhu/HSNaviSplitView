273423e4c6604408c700b75cefe1a15eb20fc6e7
hu shouwen <hesper.hu@gmail.com>
2023-06-24 15:37:36 +0800

    Initial Commit

---
+++ HSNaviSplitView/HSNaviSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSNaviSplitViewApp.swift
+//  HSNaviSplitView
+//
+//  Created by hsw on 2023-06-24.
+//
+
+import SwiftUI
+
+@main
+struct HSNaviSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
